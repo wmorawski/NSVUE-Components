8ec7c318b27e0fcf0c8d274579c02188e4a9f1e5
saibbyweb <suhaibzreason@gmail.com>
2019-01-19 00:28:20 +0530

    places API working!

--- app/pages/PlacesAPI.vue
+++ app/pages/PlacesAPI.vue
@@ -1,7 +1,7 @@
 <template>
   <Page>
     <StackLayout>
-      <!-- <button @tap="pickPlace">Pick Place</button> -->
+      <button @tap="pickPlace">Pick Place</button>
     </StackLayout>
   </Page>
 </template>
@@ -12,13 +12,10 @@ import * as GooglePlaces from "nativescript-plugin-gplaces";
 export default {
   methods: {
     pickPlace() {
-      //   GooglePlaces.init();
+      GooglePlaces.pickPlace()
+        .then(place => console.log(JSON.stringify(place)))
+        .catch(error => console.log(error));
     }
-  },
-  created: function() {
-    GooglePlaces.pickPlace()
-      .then(place => console.log(JSON.stringify(place)))
-      .catch(error => console.log(error));
   }
 };
 </script>
